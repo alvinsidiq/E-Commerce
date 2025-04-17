1e456bd87fcf79d9cee4a3353ef7858b8d276acc
unknown <alvinsidiq2001@gmail.com>
2025-04-17 21:52:57 +0800

    pemasangan frontend-user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,36 @@
 <?php
+use App\Livewire\Homepage;
+use App\Livewire\CategoriesPage;
+use App\livewire\ProductsPage;
+use App\livewire\CartPage;
+use App\livewire\ProductDetailPage;
+use App\livewire\CheckoutPage;
+use App\livewire\MyOrdersPage;
+use App\livewire\MyOrderDetailPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ForgetPasswordPage;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\SuccessPage;
+use App\Livewire\CancelPage;
 
-use Illuminate\Support\Facades\Route;
+Route::get('/success', SuccessPage::class);
+Route::get('/cancel', CancelPage::class);
+Route::get('/login',LoginPage::class);
+Route::get('/register',RegisterPage::class);
+Route::get('/forget-password',ForgetPasswordPage::class);
+Route::get('/reset-password',ResetPasswordPage::class);
+Route::get('/',Homepage::class);
+Route::get('/categories',CategoriesPage::class);
+Route::get('/products',ProductsPage::class);
+Route::get('/cart',CartPage::class);
+Route::get('/product/{id}',ProductDetailPage :: class);
+Route::get('/checkout', CheckoutPage :: class);
+Route::get('/my-orders', MyOrdersPage :: class);
+Route::get ('/my-orders/{id}', MyOrderDetailPage::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
